6754cf53ab9cc6523c723c1c7543857571a2fef7
Иванова Дарья <dashylyaiv@icloud.com>
2025-02-27 20:39:08 +0300

    Update 2.php

--- 2.php
+++ 2.php
@@ -6,16 +6,13 @@ $input = trim(fgets(STDIN));
 // Преобразуем ввод в массив чисел
 $numbers = array_map('intval', explode(' ', $input));
 
-// Удаляем дубликаты из массива
-$uniqueNumbers = array_unique($numbers);
-
 // Сортируем массив по убыванию
-rsort($uniqueNumbers);
+rsort($numbers);
 
 // Проверяем, есть ли третье максимальное число
-if (count($uniqueNumbers) >= 3) {
-    $thirdMax = $uniqueNumbers[2]; // Третье максимальное число
+if (count($numbers) >= 3) {
+    $thirdMax = $numbers[2]; // Третье максимальное число
     echo "Третье максимальное число: " . $thirdMax . "\n";
 } else {
-    echo "В массиве меньше трех уникальных чисел.\n";
+    echo "В массиве меньше трех чисел.\n";
 }
